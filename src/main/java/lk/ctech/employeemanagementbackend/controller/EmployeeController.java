54c605fc8228599e2c98a051916cf47174fc0104
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-27 19:34:30 +0530

    apply get all employees

--- src/main/java/lk/ctech/employeemanagementbackend/controller/EmployeeController.java
+++ src/main/java/lk/ctech/employeemanagementbackend/controller/EmployeeController.java
@@ -1,9 +1,23 @@
 package lk.ctech.employeemanagementbackend.controller;
 
+import lk.ctech.employeemanagementbackend.model.Employee;
+import lk.ctech.employeemanagementbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
 }
