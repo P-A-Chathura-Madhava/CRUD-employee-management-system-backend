75cc38d0387ea0300f74879028580912506d10e8
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-27 21:21:49 +0530

    apply get all employees feature

--- src/main/java/lk/ctech/employeemanagementbackend/controller/EmployeeController.java
+++ src/main/java/lk/ctech/employeemanagementbackend/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package lk.ctech.employeemanagementbackend.controller;
 import lk.ctech.employeemanagementbackend.model.Employee;
 import lk.ctech.employeemanagementbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
