afa789b8566c8c8b112a5a3cccbeb7128b1509d4
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-28 10:11:33 +0530

    apply get an employee feature

--- src/main/java/lk/ctech/employeemanagementbackend/controller/EmployeeController.java
+++ src/main/java/lk/ctech/employeemanagementbackend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package lk.ctech.employeemanagementbackend.controller;
 
+import lk.ctech.employeemanagementbackend.exception.ResourceNotFoundException;
 import lk.ctech.employeemanagementbackend.model.Employee;
 import lk.ctech.employeemanagementbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +25,12 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Integer id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        return ResponseEntity.ok(employee);
     }
 }
